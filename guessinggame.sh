e1705f63706be6e2d894129ceab207104425ba60
Z-BlackSpade7126 <163747646+Z-BlackSpade7126@users.noreply.github.com>
2024-11-14 12:32:22 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,24 @@
+congrats() {
+    echo "Congratulations! You have given the right answer!"
+}
 
+control=0
+
+while [[ $control -eq 0 ]]
+do
+    echo "What is the number of files in the current directory:"
+    read response
+    numfiles=$(ls -1 | wc -l)
+
+    echo "You entered: $response"
+    echo "The number of files is: $numfiles"
+
+    if [[ $response -eq $numfiles ]]; then
+        congrats
+        control=1
+    elif [[ $response -gt $numfiles ]]; then
+        echo "You guessed a number too high"
+    else
+        echo "You guessed a number too low"
+    fi
+done
